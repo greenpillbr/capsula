bbf1bfe9e96de36caf7b5057bdf4aad254068382
Guilherme Ferreira <gferreira525@gmail.com>
2026-05-19 19:07:02 -0300

    add talent metatag to website

--- admin-app/app/layout.tsx
+++ admin-app/app/layout.tsx
@@ -23,6 +23,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={`${geistSans.variable} h-full`}>
+      <head>
+        <meta name="talentapp:project_verification" content="d4a687caadb48f873943a77e976b6a2dd685bd627427e6c0e4f7613e373791b37d5c2a202ad8575a5a9fd42741ceee30b8e1a38b89b62b7dd6896ab6cadffc87" />
+      </head>
       <body className="min-h-full bg-white font-sans text-[#00122E] antialiased">
         <Providers>
           <Header />
