0d67a104a6c163d10a9ac60b3da704b3c2c153a2
Guilherme Ferreira <gferreira525@gmail.com>
2026-05-19 17:45:36 -0300

    fix wagmi version to compatible with rainbowkit 2

--- admin-app/lib/wagmi.ts
+++ admin-app/lib/wagmi.ts
@@ -1,9 +1,13 @@
 import { getDefaultConfig } from "@rainbow-me/rainbowkit";
 import { celo } from "wagmi/chains";
+import { http } from "wagmi";
 
 export const wagmiConfig = getDefaultConfig({
   appName: "Capsule Admin",
   projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID ?? "",
   chains: [celo],
+  transports:  {
+   [celo.id]: http("https://forno.celo.org"),
+  },
   ssr: true,
 });
